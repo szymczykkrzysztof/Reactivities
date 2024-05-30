57254f72e0357fcff0b651e87b4345f7ae97206b
Krzysztof Szymczyk <Krzysztof.Szymczyk@fenergo.com>
2024-05-30 15:53:44 +0200

    Section 66 completed

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -15,6 +15,7 @@ interface Props {
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
 export default function ActivityDashboard({
@@ -26,12 +27,14 @@ export default function ActivityDashboard({
                                               openForm,
                                               closeForm,
                                               createOrEdit,
-                                              deleteActivity
+                                              deleteActivity,
+                                              submitting
                                           }: Props) {
     return (
         <Grid>
             <Grid.Column width='10'>
-                <ActivityList activities={activities} selectActivity={selectActivity} deleteActivity={deleteActivity}/>
+                <ActivityList activities={activities} selectActivity={selectActivity} deleteActivity={deleteActivity}
+                              submitting={submitting}/>
             </Grid.Column>
             <Grid.Column width='6'>
                 {selectedActivity && !editMode &&
@@ -42,7 +45,8 @@ export default function ActivityDashboard({
 
                     />}
                 {editMode &&
-                    <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit}/>}
+                    <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit}
+                                  submitting={submitting}/>}
             </Grid.Column>
         </Grid>
     )
