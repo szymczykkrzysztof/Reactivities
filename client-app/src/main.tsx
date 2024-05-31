1686679a04dd9fcd23e96aeb1373148d988badf2
Krzysztof Szymczyk <Krzysztof.Szymczyk@fenergo.com>
2024-05-31 15:21:53 +0200

    section 77 completed

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,15 +1,11 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app/layout/App.tsx'
 import 'semantic-ui-css/semantic.min.css'
 import './app/layout/styles.css'
-import { StoreContext, store } from './app/stores/store.ts'
+import {StoreContext, store} from './app/stores/store.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-      <StoreContext.Provider value={store}>
-          <App />
-      </StoreContext.Provider>
-   
-  </React.StrictMode>,
+    <StoreContext.Provider value={store}>
+        <App/>
+    </StoreContext.Provider>
 )
