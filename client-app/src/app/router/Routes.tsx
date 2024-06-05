a83d6e023b9b7dcef9eef628b66f022fcc5a22a9
Krzysztof Szymczyk <Krzysztof.Szymczyk@fenergo.com>
2024-06-05 14:01:55 +0200

    section 114 completed

--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -1,10 +1,12 @@
-﻿import {RouteObject, createBrowserRouter} from "react-router-dom";
+﻿import {Navigate, RouteObject, createBrowserRouter} from "react-router-dom";
 import App from "../layout/App";
 import HomePage from "../../features/home/HomePage";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import ActivityForm from "../../features/activities/form/ActivityForm";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
 import TestErrors from "../../features/errors/TestError";
+import NotFound from "../../features/errors/NotFound";
+import ServerError from "../../features/errors/ServerError";
 
 export const routes: RouteObject[] = [
     {
@@ -17,6 +19,9 @@ export const routes: RouteObject[] = [
             {path: 'createActivity', element: <ActivityForm key='create'/>},
             {path: 'manage/:id', element: <ActivityForm key='manage'/>},
             {path: 'errors', element: <TestErrors/>},
+            {path: 'not-found', element: <NotFound/>},
+            {path: 'server-Error', element: <ServerError/>},
+            {path: '*', element: <Navigate replace to='/not-found'/>},
         ]
     },
 ]
--- client-app/src/features/errors/TestError.tsx
+++ client-app/src/features/errors/TestError.tsx
@@ -1,8 +1,11 @@
 ﻿import {Button, Header, Segment} from "semantic-ui-react";
 import axios from 'axios';
+import {useState} from "react";
+import ValidationError from "./ValidationError";
 
 export default function TestErrors() {
     const baseUrl = 'http://localhost:5000/api/'
+    const [errors, setErrors] = useState(null)
 
     function handleNotFound() {
         axios.get(baseUrl + 'buggy/not-found').catch(err => console.log(err.response));
@@ -25,22 +28,23 @@ export default function TestErrors() {
     }
 
     function handleValidationError() {
-        axios.post(baseUrl + 'activities', {}).catch(err => console.log(err));
+        axios.post(baseUrl + 'activities', {}).catch(err => setErrors(err));
     }
 
     return (
         <>
-            <Header as='h1' content='Test Error component' />
+            <Header as='h1' content='Test Error component'/>
             <Segment>
                 <Button.Group widths='7'>
-                    <Button onClick={handleNotFound} content='Not Found' basic primary />
-                    <Button onClick={handleBadRequest} content='Bad Request' basic primary />
-                    <Button onClick={handleValidationError} content='Validation Error' basic primary />
-                    <Button onClick={handleServerError} content='Server Error' basic primary />
-                    <Button onClick={handleUnauthorised} content='Unauthorised' basic primary />
-                    <Button onClick={handleBadGuid} content='Bad Guid' basic primary />
+                    <Button onClick={handleNotFound} content='Not Found' basic primary/>
+                    <Button onClick={handleBadRequest} content='Bad Request' basic primary/>
+                    <Button onClick={handleValidationError} content='Validation Error' basic primary/>
+                    <Button onClick={handleServerError} content='Server Error' basic primary/>
+                    <Button onClick={handleUnauthorised} content='Unauthorised' basic primary/>
+                    <Button onClick={handleBadGuid} content='Bad Guid' basic primary/>
                 </Button.Group>
             </Segment>
+            {errors && <ValidationError errors={errors}/>}
         </>
     )
 }
